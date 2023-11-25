e6c91d4039c4f9ba74953cda9d832050ae1b5d22
raegun2 <148833221+raegun2@users.noreply.github.com>
2023-11-25 17:44:55 +1000

    Delete vs codoing directory

--- vs codoing/Nike shoes_practice/index.js
+++
@@ -1,7 +0,0 @@
-let errorParagraph = document.getElementById("error")
-console.log(errorParagraph)
-
-function purchase() {
-    console.log("button clicked")
-    errorParagraph.innerText = "Something went wrong, please try again"
-}
--- vs codoing/practice/text.js
+++
@@ -1,8 +0,0 @@
-const recipient = "james"
-const sender = "Lee"
-const email = `
-hey ${recipient}! 
-
-how is it going? cheers ${sender}`
-
-console.log(email)
